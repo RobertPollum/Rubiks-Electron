2c89da210674333db52d5ca525e72c4b73aa37d7
Under Penalty <underpenalty@gmail.com>
2025-09-02 22:24:41 -0400

    Adding forge.config.js updates that didn't originally get added

--- forge.config.js
+++ forge.config.js
@@ -9,7 +9,9 @@ module.exports = {
   makers: [
     {
       name: '@electron-forge/maker-squirrel',
-      config: {},
+      config: {
+        "name": "electron_quick_start"
+      },
     },
     {
       name: '@electron-forge/maker-zip',
